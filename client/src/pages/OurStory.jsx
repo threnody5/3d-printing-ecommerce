740e9c90da3b0c2a7e7e8885558703c4eeadafb7
William <williamw15@protonmail.com>
2023-09-22 20:38:51 -0400

    add our story message to OurStory component

--- client/src/pages/OurStory.jsx
+++ client/src/pages/OurStory.jsx
@@ -1,5 +1,78 @@
+import './styles.css';
+
 const OurStory = () => {
-  return <main>Our Story</main>;
+  return (
+    <main className='our-story-container'>
+      <div className='our-story-container__div'>
+        <p className='our-story-container__paragraph'>
+          At Maker Market, our journey began with a passion for innovation and a
+          profound belief in the transformative power of 3D printing technology.
+          Founded by a group of dedicated makers and tech enthusiasts, our
+          company was born out of a shared vision to democratize the world of
+          fabrication and unleash the creative potential of individuals,
+          hobbyists, educators, and businesses alike.
+        </p>
+      </div>
+      <div className='our-story-container__div'>
+        <p className='our-story-container__paragraph'>
+          Our story begins with a small workshop where we tinkered, prototyped,
+          and experimented with 3D printers. We were captivated by the endless
+          possibilities this technology offered, from crafting intricate
+          prototypes to producing custom-designed products. As we delved deeper
+          into the world of 3D printing, we realized the immense potential it
+          held to revolutionize industries, from manufacturing to healthcare,
+          and even the way we approach learning.
+        </p>
+      </div>
+      <div className='our-story-container__div'>
+        <p className='our-story-container__paragraph'>
+          Driven by our commitment to making this groundbreaking technology
+          accessible to all, we founded Maker Market. From our humble
+          beginnings, we've grown into a trusted and reputable provider of 3D
+          printers and supplies. Our mission is clear: to empower creators of
+          all backgrounds, whether they are seasoned professionals or aspiring
+          beginners, by providing them with the tools and expertise they need to
+          bring their ideas to life.
+        </p>
+      </div>
+      <div className='our-story-container__div'>
+        <p className='our-story-container__paragraph'>
+          At Maker Market, we take pride in curating a wide selection of
+          top-quality 3D printers, filaments, and accessories, all carefully
+          chosen to meet the diverse needs of our customers. Our team of experts
+          is always on hand to offer guidance, answer questions, and share their
+          knowledge, ensuring that our customers have a seamless experience from
+          start to finish.
+        </p>
+      </div>
+      <div className='our-story-container__div'>
+        <p className='our-story-container__paragraph'>
+          Over the years, our commitment to excellence, customer satisfaction,
+          and the endless pursuit of innovation has allowed Maker Market to
+          become a leading force in the 3D printing industry. We've had the
+          privilege of witnessing the remarkable creations that our customers
+          have brought to life, from intricate art pieces to life-changing
+          inventions.
+        </p>
+      </div>
+      <div className='our-story-container__div'>
+        <p className='our-story-container__paragraph'>
+          As we continue to grow and evolve, our core values remain unchanged.
+          We are dedicated to fostering a community of makers and innovators who
+          share our passion for 3D printing. Maker Market is more than just a
+          company; it's a hub for creativity, a source of inspiration, and a
+          partner in your journey to transform imagination into reality.
+        </p>
+      </div>
+      <div className='our-story-container__div'>
+        <p className='our-story-container__paragraph'>
+          Join us at Maker Market, where the future is crafted, one layer at a
+          time. Together, we'll shape a world where innovation knows no bounds,
+          and anyone can become a maker of extraordinary things.
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default OurStory;
