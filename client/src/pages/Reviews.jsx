6f18e16ee74579aa713d59ab82830288871fd075
William <williamw15@protonmail.com>
2023-11-15 23:58:15 -0500

    update reviews and styling

--- client/src/pages/Reviews.jsx
+++ client/src/pages/Reviews.jsx
@@ -1,5 +1,77 @@
+import './styles.css';
+
 const Reviews = () => {
-  return <main>Reviews</main>;
+  return (
+    <main className='reviews-container'>
+      <div>
+        <h3 className='reviews-container__h3'>
+          Exceptional Products and Support! ⭐⭐⭐⭐⭐
+        </h3>
+        <p className='reviews-container__paragraph'>
+          I recently purchased a 3D printer and supplies from Maker Market, and
+          I couldn't be happier with my experience. The printer itself is
+          top-notch and has exceeded my expectations. What truly sets Maker
+          Market apart is their outstanding customer service. They patiently
+          answered all my questions, provided helpful tips, and ensured that I
+          had a smooth setup process. Kudos to the Maker Market team for their
+          exceptional products and support!
+        </p>
+      </div>
+      <div>
+        <h3 className='reviews-container__h3'>
+          A Game Changer for Our School ⭐⭐⭐⭐⭐
+        </h3>
+        <p className='reviews-container__paragraph'>
+          Our school decided to invest in 3D printers for our STEM program, and
+          Maker Market was the perfect choice. Not only did they provide us with
+          high-quality printers and supplies, but they also offered
+          comprehensive training for our teachers. Their dedication to education
+          and their commitment to customer satisfaction are truly commendable.
+          Our students are now creating amazing projects thanks to Maker
+          Market's products and guidance.
+        </p>
+      </div>
+      <div>
+        <h3 className='reviews-container__h3'>
+          Reliable and Dependable ⭐⭐⭐⭐⭐
+        </h3>
+        <p className='reviews-container__paragraph'>
+          I've been a loyal customer of Maker Market for years, and I can
+          confidently say that their 3D printers and supplies are some of the
+          most reliable on the market. What's equally impressive is their
+          customer service. Whenever I've had an issue or needed assistance,
+          their team has been quick to respond and always goes above and beyond
+          to help. Maker Market has earned my trust and loyalty.
+        </p>
+      </div>
+      <div>
+        <h3 className='reviews-container__h3'>
+          Outstanding Filaments! ⭐⭐⭐⭐⭐
+        </h3>
+        <p className='reviews-container__paragraph'>
+          I've tried various 3D printer filaments, but Maker Market's selection
+          stands out. Their filaments are of exceptional quality and
+          consistency, resulting in stunning prints every time. Their range of
+          colors and materials is impressive, and their fast shipping ensures I
+          never run out. If you're a 3D printing enthusiast, Maker Market is the
+          place to shop for your filament needs.
+        </p>
+      </div>
+      <div>
+        <h3 className='reviews-container__h3'>
+          Customer-Centric Excellence ⭐⭐⭐⭐⭐
+        </h3>
+        <p className='reviews-container__paragraph'>
+          Maker Market truly understands the needs of their customers. Their
+          website is user-friendly, their product descriptions are informative,
+          and their support team is always ready to assist. Whether you're a
+          beginner or an expert in 3D printing, Maker Market caters to everyone.
+          Their dedication to customer-centric excellence is why I keep coming
+          back.
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default Reviews;
