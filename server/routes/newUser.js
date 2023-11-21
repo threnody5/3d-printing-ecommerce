a0303a20d42fd300c82becc418ae45dd10eb5728
William <williamw15@protonmail.com>
2023-11-21 00:27:01 -0500

    user can create account, emailAddress and password are saved on database under users table.

--- server/routes/newUser.js
+++ server/routes/newUser.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql2');
 
-//TODO: Create functions for saving the data into the database with mysql2.
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+console.log(process.env.MYSQL_HOST);
+
 //TODO: When password is salted, unsalt password here. Research if unsalted password should be stored in the database, I doubt that though.
 router.post('/create-user', (req, res, next) => {
   const { emailAddress, password } = req.body;
 
-  //TODO: If account is successfully created on MySQL database, this is response to front end.
-  res.status(201).send({ message: 'Account successfully created.' });
+  const data = {
+    userID: emailAddress,
+    userPassword: password,
+  };
+
+  connection.query(
+    `INSERT INTO users SET ?`,
+    data,
+    (error, results, fields) => {
+      if (error) {
+        console.error('Error Adding User: ', error);
+        res.status(409).send({ message: 'Error Adding User.' });
+      } else {
+        console.log('User Successfully Added.');
+        res.status(201).send({ message: 'User Successfully Added.' });
+        console.log('Results: ', results);
+      }
+    }
+  );
 
   //TODO: If account already exists, send error 409 with message.
 });
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 const imageRouter = require('./routes/images');
 const userRouter = require('./routes/newUser');
 
