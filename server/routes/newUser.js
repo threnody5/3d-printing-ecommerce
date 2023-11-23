efd56856e1d317ab10d05daf1d8f233a9a0690aa
William <williamw15@protonmail.com>
2023-11-22 22:39:08 -0500

    added salting of password before being saved to database

--- server/routes/newUser.js
+++ server/routes/newUser.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
@@ -9,33 +11,37 @@ const connection = mysql.createConnection({
   database: process.env.MYSQL_DATABASE,
 });
 
-console.log(process.env.MYSQL_HOST);
-
 //TODO: When password is salted, unsalt password here. Research if unsalted password should be stored in the database, I doubt that though.
 router.post('/create-user', (req, res, next) => {
   const { emailAddress, password } = req.body;
 
-  const data = {
-    userID: emailAddress,
-    userPassword: password,
-  };
+  bcrypt.hash(password, saltRounds, (error, hash) => {
+    if (error) {
+      console.error('Error Hashing Password: ', error);
+      res
+        .status(500)
+        .send({ message: 'Internal Server Error. Please try again. ' });
+    } else {
+      const hashedPassword = hash;
 
-  connection.query(
-    `INSERT INTO users SET ?`,
-    data,
-    (error, results, fields) => {
-      if (error) {
-        console.error('Error Adding User: ', error);
-        res.status(409).send({ message: 'Error Adding User.' });
-      } else {
-        console.log('User Successfully Added.');
-        res.status(201).send({ message: 'User Successfully Added.' });
-        console.log('Results: ', results);
-      }
+      const data = {
+        userID: emailAddress,
+        userPassword: hashedPassword,
+      };
+
+      connection.query(`INSERT INTO users SET ?`, data, (error, results) => {
+        if (error) {
+          //TODO: Figure out a way to specify if the email address already exists as a user name, if so then send an error back to the user.
+          console.error('Error Adding User: ', error);
+          res.status(409).send({ message: 'Error Adding User.' });
+        } else {
+          console.log('User Successfully Added.');
+          res.status(201).send({ message: 'User Successfully Added.' });
+          console.log('Results: ', results);
+        }
+      });
     }
-  );
-
-  //TODO: If account already exists, send error 409 with message.
+  });
 });
 
 module.exports = router;
